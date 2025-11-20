b78912987f0e0f9331746c74c196b66cc7668220
Sébastien Dubois <sebastiendubois@wifi-eduroam-interne-172-29-113-135.wifi.unistra.fr>
2025-11-20 12:49:05 +0100

    Add Tailwind safelist to force color classes compilation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     "./app/**/*.{ts,tsx,js,jsx,css}",
     "./components/**/*.{ts,tsx,js,jsx,css}"
   ],
+  safelist: [
+    'bg-yellow-100',
+    'bg-orange-200',
+    'bg-red-200',
+    'bg-purple-200',
+    'bg-blue-200',
+    'bg-gray-100',
+  ],
   theme: { extend: {} },
   plugins: []
 };
